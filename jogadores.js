1d4d4f5acd1d6656f25db5a4fbb076b29ec12e15
guilherme <guilhermepardelhas@gmail.com>
2024-11-25 02:11:23 -0300

    ultimo

--- jogadores.js
+++ jogadores.js
@@ -1,24 +1,6 @@
 const elenco_botoes = document.getElementById('elencos')
 const container_atletas = document.getElementById('atletas')
 
-
-document.getElementById('sair').onclick = ()=>{
-    window.location.href = 'index.html'
-}
-
-document.getElementById('pesquisa').addEventListener('input', (e)=>{
-    const value = e.target.value
-    const atletasFiltro = atletas.filter(
-        (atleta) => atleta.nome.toLowerCase().includes(value.toLowerCase())
-    )
-    atualiza_atletas(atletasFiltro)
-
-})
-
-
-let atletas = []
-
-
 const criaBotao = (title, id, elenco ) => {
     const btnM = document.createElement("button")
     btnM.textContent = title
@@ -34,26 +16,26 @@ const criaBotao = (title, id, elenco ) => {
 const criaCardAtleta = (atleta) => {
     const cardAtleta = document.createElement("div");
     cardAtleta.className = "card_atleta";
-
+    
     const imagem = document.createElement("img");
     imagem.src = atleta.imagem;
     imagem.className = "imagem";
-
+    
     const nome = document.createElement('h2')
     nome.textContent = atleta.nome
     nome.className = "nome"
-
+    
     const saibaMais = document.createElement('button')
     saibaMais.textContent = 'Saiba mais'
     saibaMais.className = 'saibaMais'
     saibaMais.onclick = () => {
         window.location.href = `detalhes.html?id=${atleta.id}`
     }
-
+    
     cardAtleta.appendChild(imagem)
     cardAtleta.appendChild(nome)
     cardAtleta.appendChild(saibaMais)
-
+    
     container_atletas.appendChild(cardAtleta);
 };
 
@@ -61,31 +43,72 @@ criaBotao('Masculino', 'masc', 'masculino')
 criaBotao('Feminino', 'fem', 'feminino')
 criaBotao('Todes', 'all', 'all')
 
+const masculino = document.getElementById ("masc")
+const feminino = document.getElementById ("fem")
+const todes = document.getElementById ("all")
+
+if (sessionStorage.getItem('logado')) {
+
+document.getElementById('sair').onclick = () => {sessionStorage.removeItem('logado'); window.location = "index.html"}
+
+document.getElementById('pesquisa').addEventListener('input', (e)=>{
+    const value = e.target.value
+    const atletasFiltro = atletas.filter(
+        (atleta) => atleta.nome.toLowerCase().includes(value.toLowerCase())
+    )
+    atualiza_atletas(atletasFiltro)
+
+})
+
+
+let atletas = []
 
 const fetchData = async (endPoint) => {
     try {
-      const response = await fetch("https://botafogo-atletas.mange.li/2024-1/" + endPoint);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      return data;
+        const response = await fetch("https://botafogo-atletas.mange.li/2024-1/" + endPoint);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
     } catch (error) {
-      console.error('Error fetching data:', error);
-      throw error;
+        console.error('Error fetching data:', error);
+        throw error;
     }
   }
+  
+  const atualiza_atletas = (atletas) => {
+      container_atletas.innerHTML = ""
+      atletas.forEach(element => {
+          criaCardAtleta(element)
+        });
+    }
+    
+    const busca_atletas = async (elenco)=>{
+        atletas = await fetchData(elenco)
+        atualiza_atletas(atletas)
+    }
 
-const atualiza_atletas = (atletas) => {
-    container_atletas.innerHTML = ""
-    atletas.forEach(element => {
-        criaCardAtleta(element)
-    });
+    masculino.addEventListener("click", function(){ 
+        const elenco = "masculino"
+        busca_atletas(elenco)
+
+    })
+    feminino.addEventListener("click", function(){ 
+        const elenco = "feminino"
+        busca_atletas(elenco)
+
+    })
+    todes.addEventListener("click", function(){ 
+        const elenco = "all"
+        busca_atletas(elenco)
+
+    })
+
+
+
+
+    busca_atletas('all')
+} else {
+    document.body.innerHTML = `<h3>Indisponivel, faça login</h3>`
 }
-
-const busca_atletas = async (elenco)=>{
-    atletas = await fetchData(elenco)
-    atualiza_atletas(atletas)
-}
-
-busca_atletas('all')
